c3e7742a1919756728d5205658c478962c067c8a
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-11-03 17:10:15 +0100

    MP

---
+++ src/test/java/Notlar.java
@@ -0,0 +1,61 @@
+public class Notlar {
+    /*
+    GithUb da target cakismamasi icin:
+1- Git ignore eklemek
+2- Target dosyasini silmek
+3- Terminal'de mvn clean
+
+
+Hüseyin Avni ŞEN
+  < 1 minute ago
+// response.print(); // sadce bodyi string olarak yazdırır
+// response.peek(); // tüm veriyi string olarak verir.
+
+Terminalden target i silme;mvn clean ctrl+enter
+githup a göndermeden once yapki conflict olusmasin
+
+assertThat -->hard Assert tir
+
+//  hasSize() methodu aradıgımız datadan istediğimiz yerde kaç tane oldugunu bize verir.
+//  hasItems() methodu aradıgımız datanın istenen yerde olup olmadıgını verir
+
+     */
+
+    /*
+1) Postman, manuel API testleri icin kullandik,
+2) otomasyon tetsleri icin de Reset Assured Library kullanacagiz
+3) Otomasyon testleri yaparken asagidaki adimlari izliyoruz
+    a) Gereksinimleri anlamak,
+    b) Test Case yaziyoruz
+        i) Test Case yaziminda "Gherkin" dilini kullanacagiz.
+        Bizler yazilim diline hakim olsakta karsimizdaki kisiler yazilim
+        diline hakim olmayabilirler. Oysa Gerkin dili kullanilirsa onlarda
+        anlayabilir
+
+        -Given : On kosul olusturmak icin kullanilir,
+
+        -When  : Yapilacak aksiyonlar icin kullanilir(get(), put(), patch() ve delete())
+
+        -Then  : Istek yaptiktan sonra (requets gonderdikten sonra) dogrulama kisminda
+
+        -And   : Coklu islemlerde kullanacagiz
+
+
+    c)  Test kodlarimizi yazmaya baslayacagiz
+
+        i)   Set the URL,
+        ii)  Set the expected Data (bklenen datanin olusturulmasi:Post,Put ve Patch ileolusturacagiz))
+        iii) Type code to send request(Talep gondermek icin kod yazimi)
+        iv)  Do Assertion(dogrulama yapmak icin)*/
+
+
+    /*Hata alirsan pom Xml e bunu kur
+    <!-- https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind -->
+<dependency>
+    <groupId>com.fasterxml.jackson.core</groupId>
+    <artifactId>jackson-databind</artifactId>
+    <version>2.14.0-rc3</version>
+</dependency>
+     */
+
+}
---
+++ src/test/java/day01/GetRequest03.java
@@ -0,0 +1,93 @@
+package day01;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class GetRequest03 {
+     /* Matchers ile dataları doğrulayınız
+             "id": 5,
+            "email": "charles.morris@reqres.in",
+            "first_name": "Charles",
+            "last_name": "Morris",
+            "avatar": "https://reqres.in/img/faces/5-image.jpg"
+     */
+
+    @Test
+    public void test03(){
+        String url= "https://reqres.in/img/faces/5-image.jpg";
+    }
+
+
+/*
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+public class GetRequest03 {
+    /*
+     Matchers ile dataları doğrulayınız
+    "id": 5,
+            "email": "charles.morris@reqres.in",
+            "first_name": "Charles",
+            "last_name": "Morris",
+            "avatar": "https://reqres.in/img/faces/5-image.jpg"
+     */
+    /*
+@Test
+public void test03() {
+    String url="https://reqres.in/api/users";
+    Response response = given().when().get(url);
+    response.then().assertThat().
+            statusCode(200).
+            statusLine("HTTP/1.1 200 OK").
+            contentType(ContentType.JSON);
+    response.then().body("data[4].email", equalTo("charles.morris@reqres.in"));
+    response.then().body("data[4].first_name", equalTo("Charles"));
+    response.then().body("data[4].last_name", equalTo("Morris"));
+    response.then().body("data[4].avatar", equalTo("https://reqres.in/img/faces/5-image.jpg"));
+}
+}
+ */
+
+
+
+        /*
+          Matchers ile dataları doğrulayınız
+                "id": 5,
+                "email": "charles.morris@reqres.in",
+                "first_name": "Charles",
+                "last_name": "Morris",
+                "avatar": "https://reqres.in/img/faces/5-image.jpg"
+         */
+
+
+
+    @Test
+    public void test04() {
+        String url = "https://reqres.in/api/users";
+
+        Response response = given().when().get(url);
+        //response.prettyPeek();
+        response.prettyPrint();
+
+        /*"id": 5,
+                "email": "charles.morris@reqres.in",
+                "first_name": "Charles",
+                "last_name": "Morris",
+                "avatar": "https://reqres.in/img/faces/5-image.jpg"
+         */
+
+        // Matchers ile id : 5 olan dataları doğrulayınız
+
+        response.then().body("data[4].email", equalTo("charles.morris@reqres.in"),
+                "data[4].first_name", equalTo("Charles"), "data[4].avatar",
+                equalTo("https://reqres.in/img/faces/5-image.jpg"));
+
+    }
+
+}
